4304c1e7370d7ca37abe6933d2c66dd75ed83597
Luqman Abyadh <lqmn.abydh@gmail.com>
2019-12-09 01:22:52 +0700

    ya

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-//variabel untuk memasukkan username
+// id variabel untuk memasukkan username
 let username = document.getElementById("username");
-//variable untuk memindahkan halaman pertama ke halaman ke dua
+// id variable untuk memindahkan halaman pertama ke halaman ke dua
 let halamanpertama = document.getElementById("halamanpertama");
 let halamandua = document.getElementById("halamandua");
-//untuk memunculkan username nya lagi
+// id untuk memunculkan username nya lagi
 let usernamedisplay = document.getElementById("usernamedisplay");
-
+// id untuk square button
+let usernamedisplay2 = document.getElementById("usernamedisplay2")
 
 //function untuk save username saat di click tombol go
 function saveusername() {
@@ -23,23 +24,59 @@ function saveusername() {
     }
 }
 
-function kembalikehalamansatu() {
+// function untuk tombol shape
+
+function squarebutton() {
+    window.location.href = "kotak.html";
+    usernamedisplay2.innerHTML = localStorage.getItem('usernamedisplay');
+}
+
+// function untuk tombol back dan end session
+function backbutton() {
     halamanpertama.style.display = 'inline';
     halamandua.style.display = 'none';
 }
 
-function kliktombolsquare() {
-    window.location.href = "kotak.html"
+function endsessionbutton() {
+    window.location.href = "home.html";
 }
 
-function kliktombolround() {
+function newshapebutton() {
+    window.location.href = "home.html"; {
+        halamanpertama.style.display = 'none';
+        halamandua.style.display = 'inline';
+    }
+}
+
+// function untuk formula
+
+//function kliktombolcountsquare() {
+
+// }
+
+// function kliktombolcountrectangle() {
+
+// }
+
+// function kliktombolcounttriangle() {
+
+// }
+
+// function kliktombolcountround() {
+
+// }
+
+//function untuk tombol shape
+
+
+function roundbutton() {
     window.location.href = "lingkaran.html"
 }
 
-function kliktomboltriangle() {
+function trianglebutton() {
     window.location.href = "segitiga.html"
 }
 
-function kliktombolrectangle() {
+function rectanglebutton() {
     window.location.href = "persegipanjang.html"
 }
